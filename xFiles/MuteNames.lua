5b4989206812635c7df747ce289f22065406232e
DevXTeam0 <87077696+DevXTeam0@users.noreply.github.com>
2021-07-07 18:26:24 +0300

    Update MuteNames.lua

--- xFiles/MuteNames.lua
+++ xFiles/MuteNames.lua
@@ -8,25 +8,25 @@ message_ids_=id
 },func or dl_cb,nil)
 end
 if ChatType == 'sp' or ChatType == 'gp'  then
-if DevRio:get(x.."Rio:Lock:MuteNames"..msg.chat_id_) then
+if DevRio:get(DevX.."Rio:Lock:MuteNames"..msg.chat_id_) then
 if Manager(msg) then
 if text and (text:match("^كتم اسم (.*)$") or text:match("^كتم الاسم (.*)$")) then
 local MuteName = text:match("^كتم اسم (.*)$") or text:match("^كتم الاسم (.*)$")
 send(msg.chat_id_, msg.id_, '⌁︙تم كتم الاسم ↫ '..MuteName)
-DevRio:sadd(x.."Rio:Mute:Names"..msg.chat_id_, MuteName)
+DevRio:sadd(DevX.."Rio:Mute:Names"..msg.chat_id_, MuteName)
 end
 if text and (text:match("^الغاء كتم اسم (.*)$") or text:match("^الغاء كتم الاسم (.*)$")) then
 local UnMuteName = text:match("^الغاء كتم اسم (.*)$") or text:match("^الغاء كتم الاسم (.*)$")
 send(msg.chat_id_, msg.id_, '⌁︙تم الغاء كتم الاسم ↫ '..UnMuteName)
-DevRio:srem(x.."Rio:Mute:Names"..msg.chat_id_, UnMuteName)
+DevRio:srem(DevX.."Rio:Mute:Names"..msg.chat_id_, UnMuteName)
 end
 end
 if text == "حذف الاسماء المكتومه" and Constructor(msg) or text == "مسح الاسماء المكتومه" and Constructor(msg) then
-DevRio:del(x.."Rio:Mute:Names"..msg.chat_id_)
+DevRio:del(DevX.."Rio:Mute:Names"..msg.chat_id_)
 send(msg.chat_id_, msg.id_, "⌁︙تم حذف الاسماء المكتومه")
 end
 if text == "الاسماء المكتومه" and Constructor(msg) then
-local AllNames = DevRio:smembers(x.."Rio:Mute:Names"..msg.chat_id_)
+local AllNames = DevRio:smembers(DevX.."Rio:Mute:Names"..msg.chat_id_)
 Text = "\n⌁︙قائمة الاسماء المكتومه ↫ ⤈\n┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉\n"
 for k,v in pairs(AllNames) do
 Text = Text..""..k.."~ (["..v.."])\n"
@@ -37,12 +37,12 @@ end
 send(msg.chat_id_, msg.id_, Text)
 end
 end
-if not Manager(msg) and DevRio:get(x.."Rio:Lock:MuteNames"..msg.chat_id_) then
+if not Manager(msg) and DevRio:get(DevX.."Rio:Lock:MuteNames"..msg.chat_id_) then
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
 if result.id_ then 
 xName = ((result.first_name_ or "") .. (result.last_name_ or ""))
 if xName then 
-xNames = DevRio:smembers(x.."Rio:Mute:Names"..msg.chat_id_) or ""
+xNames = DevRio:smembers(DevX.."Rio:Mute:Names"..msg.chat_id_) or ""
 if xNames and xNames[1] then 
 for i=1,#xNames do 
 if xName:match("(.*)("..xNames[i]..")(.*)") then 
@@ -58,16 +58,16 @@ end
 if Constructor(msg) then
 if text == "تفعيل كتم الاسم" or text == "تفعيل كتم الاسماء" then
 send(msg.chat_id_, msg.id_, '⌁︙تم التفعيل سيتم كتم العضو الذي يضع الاسماء المكتومه')
-DevRio:set(x.."Rio:Lock:MuteNames"..msg.chat_id_,true)
+DevRio:set(DevX.."Rio:Lock:MuteNames"..msg.chat_id_,true)
 end
 if text == "تعطيل كتم الاسم" or text == "تعطيل كتم الاسماء" then
 send(msg.chat_id_, msg.id_, '⌁︙تم تعطيل سيتم كتم العضو الذي يضع الاسماء المكتومه')
-DevRio:del(x.."Rio:Lock:MuteNames"..msg.chat_id_)
+DevRio:del(DevX.."Rio:Lock:MuteNames"..msg.chat_id_)
 end
 end
 end
 
 end
 return {
-x = MuteNames,
+DevX = MuteNames,
 }
