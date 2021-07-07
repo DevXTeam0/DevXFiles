a91ce6d081bd77a01fd0eccb20bc5c39d3fe6cb6
DevXTeam0 <87077696+DevXTeam0@users.noreply.github.com>
2021-07-07 18:22:47 +0300

    Update AddedMe.lua

--- xFiles/AddedMe.lua
+++ xFiles/AddedMe.lua
@@ -2,13 +2,13 @@ local function AddedMe(msg)
 local text = msg.content_.text_
 if ChatType == 'sp' or ChatType == 'gp'  then
 if text and text:match("منو ضافني") then
-if not DevRio:get(x..'Rio:Added:Me'..msg.chat_id_) then
+if not DevRio:get(DevX..'Rio:Added:Me'..msg.chat_id_) then
 tdcli_function ({ID = "GetChatMember",chat_id_ = msg.chat_id_,user_id_ = msg.sender_user_id_},function(arg,da) 
 if da and da.status_.ID == "ChatMemberStatusCreator" then
 Dev_Rio(msg.chat_id_, msg.id_, 1, '⌁︙انت منشئ المجموعه', 1, 'md') 
 return false
 end
-local Added_Me = DevRio:get(x.."Who:Added:Me"..msg.chat_id_..':'..msg.sender_user_id_)
+local Added_Me = DevRio:get(DevX.."Who:Added:Me"..msg.chat_id_..':'..msg.sender_user_id_)
 if Added_Me then 
 tdcli_function ({ID = "GetUser",user_id_ = Added_Me},function(extra,result,success)
 local Name = '['..result.first_name_..'](tg://user?id='..result.id_..')'
@@ -25,16 +25,16 @@ end
 end
 
 if text == 'تفعيل ضافني' and Manager(msg) then 
-DevRio:del(x..'Rio:Added:Me'..msg.chat_id_) 
+DevRio:del(DevX..'Rio:Added:Me'..msg.chat_id_) 
 Dev_Rio(msg.chat_id_, msg.id_, 1, '⌁︙تم تفعيل امر منو ضافني', 1, 'md') 
 end
 if text == 'تعطيل ضافني' and Manager(msg) then 
-DevRio:set(x..'Rio:Added:Me'..msg.chat_id_,true) 
+DevRio:set(DevX..'Rio:Added:Me'..msg.chat_id_,true) 
 Dev_Rio(msg.chat_id_, msg.id_, 1, '⌁︙تم تعطيل امر منو ضافني', 1, 'md') 
 end
 end
 
 end
 return {
-x = AddedMe
+DevX = AddedMe
 }
